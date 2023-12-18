dd86afb09e9ba17e07f5e353cef97ab740699912
PavelBokun <pavel.snabteh@gmail.com>
2023-12-18 23:00:33 +0300

    feat: add styles

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.tsx'
+
+import App from '@/App'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import './styles/index.scss'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
